87580a40e79013087fb555e40d2eb81d22d713c3
AlexShabanova <iialex.shabanovaii@gmail.com>
2026-03-02 13:13:01 +0300

    update bot config

--- app/telegram_bot/bot.py
+++ app/telegram_bot/bot.py
@@ -14,58 +14,86 @@ from aiogram.filters import CommandStart, Command
 
 import bot_config
 
-# FIXME logger = logging.getLogger(__name__)
-# BOT_TOKEN = bot_config.BOT_TOKEN
-dotenv.load_dotenv("..")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-dp = Dispatcher()
+# # FIXME logger = logging.getLogger(__name__)
+# # BOT_TOKEN = bot_config.BOT_TOKEN
+# dotenv.load_dotenv("..")
+# BOT_TOKEN = os.getenv("BOT_TOKEN")
+# dp = Dispatcher()
+#
+# menu_commands: dict[str, str] = {
+#     "/help": "Click here for help",
+#     "/post": "Write a post",
+#     "/start": "Click here for start",
+# }
+#
+#
+# async def set_main_menu(bot: Bot):
+#     main_menu_commands = [
+#         BotCommand(command=command, description=description)
+#         for command, description in menu_commands.items()
+#     ]
+#     await bot.set_my_commands(main_menu_commands)
+#
+#
+# @dp.message(CommandStart())
+# async def handle_start(message: Message):
+#     await message.answer(text=f"Hello, {message.from_user.full_name}!")
+#
+#
+# @dp.message(Command("help"))
+# async def handle_help(message: Message):
+#     text = "Это бот для ведения дневника на японском языке.\nВы пишите пост, а он переводит неизвестные слова"
+#     await message.answer(text=text)
+#
+#
+# @dp.message(CommandStart())
+# async def handle_start(message: Message):
+#     await message.answer(text=f"Hello, {message.from_user.full_name}!")
+#
+#
+# # TODO выбрать или yaml или словарь
+# # with open('logging_config.yaml', 'rt') as f:
+# #     config = yaml.safe_load(f.read())
+#
+#
+# async def main():
+#     # FIXME перенести конфиг в main
+#     logging.config.dictConfig(logging_config)
+#     bot = Bot(token=BOT_TOKEN)
+#     await set_main_menu(bot)
+#     await dp.start_polling(bot)
 
-menu_commands: dict[str, str] = {
-    "/help": "Click here for help",
-    "/post": "Write a post",
-    "/start": "Click here for start",
-}
+
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
 
 
-async def set_main_menu(bot: Bot):
-    main_menu_commands = [
-        BotCommand(command=command, description=description)
-        for command, description in menu_commands.items()
-    ]
-    await bot.set_my_commands(main_menu_commands)
+# Функция конфигурирования и запуска бота
+async def main() -> None:
 
+    # Загружаем конфиг в переменную config
+    # FIXME убрать абсолютный путь - при переносе нужно проверить загрузка в венв переменных из .env (должно вернуть True) dotenv.load_dotenv(dotenv_path='C:\\Users\\user\\PycharmProjects\\MyDuoDiary_app\\app\\.env', override=True)
+    config: bot_config.BotConfig = bot_config.load_bot_config(
+        "C:\\Users\\user\\PycharmProjects\\MyDuoDiary_app\\app\\.env"
+    )
+    # Задаём базовую конфигурацию логирования
+    logging.basicConfig(
+        level=logging.getLevelName(level=config.log.level),
+        format=config.log.format,
+    )
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.bot.token)
+    dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def handle_start(message: Message):
-    await message.answer(text=f"Hello, {message.from_user.full_name}!")
-
-
-@dp.message(Command("help"))
-async def handle_help(message: Message):
-    text = "Это бот для ведения дневника на японском языке.\nВы пишите пост, а он переводит неизвестные слова"
-    await message.answer(text=text)
-
-
-@dp.message(CommandStart())
-async def handle_start(message: Message):
-    await message.answer(text=f"Hello, {message.from_user.full_name}!")
-
-
-# TODO выбрать или yaml или словарь
-# with open('logging_config.yaml', 'rt') as f:
-#     config = yaml.safe_load(f.read())
-
-
-async def main():
-    # FIXME перенести конфиг в main
-    logging.config.dictConfig(logging_config)
-    bot = Bot(token=BOT_TOKEN)
-    await set_main_menu(bot)
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# if __name__ == "__main__":
+asyncio.run(main())
 
 
 # TODO ЛОГИ -- залогировать что-то в проекте
--- app/telegram_bot/bot_config.py
+++ app/telegram_bot/bot_config.py
@@ -1,3 +1,25 @@
 import os
+import dotenv
+from pydantic import BaseModel
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+class TgBot(BaseModel):
+    token: str
+
+
+class LogSettings(BaseModel):
+    level: str
+    format: str
+
+
+class BotConfig(BaseModel):
+    bot: TgBot
+    log: LogSettings
+
+
+def load_bot_config(path: str | None = None) -> BotConfig:
+    dotenv.load_dotenv(dotenv_path=path, override=True)
+    return BotConfig(
+        bot=TgBot(token=os.getenv("BOT_TOKEN")),
+        log=LogSettings(level=os.getenv("LOG_LEVEL"), format=os.getenv("LOG_FORMAT")),
+    )
