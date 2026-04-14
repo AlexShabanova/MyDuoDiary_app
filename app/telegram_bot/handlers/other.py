b364d69ef1170e50c7889c7328fc637789e9f1bc
AlexShabanova <iialex.shabanovaii@gmail.com>
2026-04-14 15:50:56 +0300

    update other_router

--- app/telegram_bot/handlers/other.py
+++ app/telegram_bot/handlers/other.py
@@ -1,15 +1,12 @@
 from aiogram import Router
 from aiogram.types import Message
-
-from telegram_bot.keyboards.inline_keyboards import inline_keyboard
 from telegram_bot.lexicon.lexicon import LEXICON
 
 # Инициализируем роутер уровня модуля
 other_router = Router()
 
-
 # Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
 @other_router.message()
 async def process_other_command(message: Message):
-    await message.reply(text=LEXICON["no_post"])
+    await message.answer(text=LEXICON["no_post"])
