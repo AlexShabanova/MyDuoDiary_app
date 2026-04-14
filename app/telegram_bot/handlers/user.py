5e39b10f97f6386b208a2e4034ee21dd4f6439f8
AlexShabanova <iialex.shabanovaii@gmail.com>
2026-04-14 15:53:33 +0300

    add cancel handler, delete comments

--- app/telegram_bot/handlers/user.py
+++ app/telegram_bot/handlers/user.py
@@ -9,13 +9,14 @@ from aiogram.types import (
     CallbackQuery,
 )
 
-from telegram_bot.keyboards.inline_keyboards import inline_keyboard
-from telegram_bot.keyboards import reply_keyboard
+from telegram_bot.keyboards.inline_keyboards import (
+    inline_keyboard,
+    inline_cancel_keyboard,
+)
 from telegram_bot.lexicon.lexicon import LEXICON
 from telegram_bot.states.states import FSMTranslatePost
 
 logger = logging.getLogger(__name__)
-
 user_router = Router()
 
 
@@ -30,43 +31,27 @@ async def process_start_command(message: Message):
 
 # Этот хэндлер будет срабатывать на команду "/cancel" в состоянии
 # по умолчанию и сообщать, что эта команда работает внутри машины состояний
+# FIXME этот хендлер нужен?
 @user_router.message(Command(commands="cancel"), StateFilter(default_state))
 async def process_cancel_command(message: Message):
-    await message.answer(
+    sent_msg = await message.answer(
         text="Отменять нечего. Вы вне машины состояний\n\n",
-        reply_markup=inline_keyboard,
+        # reply_markup=inline_keyboard,
     )
-
-
-# Этот хэндлер будет срабатывать на команду "/cancel" в любых состояниях,
-# кроме состояния по умолчанию, и отключать машину состояний
-@user_router.message(Command(commands="cancel"), ~StateFilter(default_state))
-async def process_cancel_command_state(message: Message, state: FSMContext):
-    await message.answer(
-        text="Вы вышли из машины состояний\n\n",
-        reply_markup=inline_keyboard,
-    )
-    # Сбрасываем состояние и очищаем данные, полученные внутри состояний
-    await state.clear()
+    await sent_msg.delete()
 
 
 # Этот хэндлер будет срабатывать на нажатие кнопки post
 # и переводить бота в состояние ожидания ввода имени
 @user_router.callback_query(F.data == "post_button_click", StateFilter(default_state))
 async def process_post_command(callback: CallbackQuery, state: FSMContext):
-    # await callback.message.answer(text="Write a post, put unfamiliar words in ()")
-    # await callback.answer(text="Write a post, put unfamiliar words in ()")
-
-    sent_msg = await callback.message.edit_text(
-        text="Write a post, put unfamiliar words in ()"
-    )
+    await callback.message.answer(text="Write a post, put unfamiliar words in ()")
 
     # Устанавливаем состояние ожидания написания поста
     await state.set_state(FSMTranslatePost.post_state)
-    # await sent_msg.delete()
-    # # Удаляем кнопку,
-    # # чтобы у пользователя не было желания тыкать кнопки
-    # await callback.message.edit_reply_markup(reply_markup=None)
+    # Удаляем кнопку,
+    # чтобы у пользователя не было желания тыкать кнопки
+    await callback.message.edit_reply_markup(reply_markup=None)
 
 
 # Этот хэндлер будет срабатывать, если написан корректный пост
@@ -78,44 +63,62 @@ async def process_post_sent(message: Message, state: FSMContext):
     # Сохраняем пост в хранилище по ключу "post"
     await state.update_data(post=message.text)
 
-    # sent_msg = await message.answer(text="Переводим пост...")
-
     # Устанавливаем состояние ожидания перевода
     await state.set_state(FSMTranslatePost.translate_state)
-
     translated_text = message.text.upper()
     await state.update_data(translated_post=translated_text)
-
-    # # удаляем предыдущее сообщение перед отправкой нового
-    # await sent_msg.delete()
-
     await message.reply(
         text=translated_text,
         reply_markup=inline_keyboard,
     )
-
     # Сбрасываем состояние и очищаем данные, полученные внутри состояний
     await state.clear()
-    # # Отправляем в чат сообщение о выходе из машины состояний
-    # await message.answer(
-    #     text="Крутой пост!\n\n" "Вы вышли из машины состояний",
-    #     reply_markup=inline_keyboard,
-    # )
+
+
+@user_router.message(StateFilter(FSMTranslatePost.post_state))
+async def warning_no_words_to_translate(message: Message):
+    await message.answer(
+        text="В посте не найдены слова, которые нужно перевести\n\n"
+        "Пожалуйста, пишите неизвестные слова в () или сохраните пост",
+        reply_markup=inline_cancel_keyboard,
+    )
 
 
 # Этот хэндлер будет срабатывать, если во время ввода поста
 # будет введено что-то некорректное
-@user_router.message(
-    StateFilter(FSMTranslatePost.post_state),
-)
+@user_router.message(StateFilter(FSMTranslatePost.post_state))
 async def warning_no_words_to_translate(message: Message):
     await message.answer(
         text="В посте не найдены слова, которые нужно перевести\n\n"
-        "Пожалуйста, пишите неизвестные слова в ()\n\n"
-        "Если вы хотите прервать написание поста - "
-        "отправьте команду /cancel"
+        "Пожалуйста, пишите неизвестные слова в () или сохраните пост",
+        reply_markup=inline_cancel_keyboard,
     )
-    # TODO две кнопки: сохранить пост и отмена
+
+
+# Этот хэндлер будет срабатывать на команду "/cancel" в любых состояниях,
+# кроме состояния по умолчанию, и отключать машину состояний
+@user_router.callback_query(
+    F.data == "cancel_button_click", ~StateFilter(default_state)
+)
+async def process_cancel_command_state(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(
+        text="Вы вышли из машины состояний\n\n",
+        reply_markup=inline_keyboard,
+    )
+    # Сбрасываем состояние и очищаем данные, полученные внутри состояний
+    await state.clear()
+
+
+@user_router.callback_query(
+    F.data == "save_button_click", StateFilter(FSMTranslatePost.post_state)
+)
+async def process_save_command_state(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(
+        text="Пост сохранен\n\n",
+        reply_markup=inline_keyboard,
+    )
+    # Сбрасываем состояние и очищаем данные, полученные внутри состояний
+    await state.clear()
 
 
 # Этот хэндлер срабатывает на команду /help
@@ -151,6 +154,3 @@ async def process_web_app_command(message: Message):
         text="tanoshii dictionary",
         reply_markup=inline_keyboard,
     )
-
-
-# TODO сделать кнопку cancel
