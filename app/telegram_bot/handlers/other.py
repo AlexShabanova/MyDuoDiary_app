6022d71b6a92c623f2554e0021cd4783497657f8
AlexShabanova <iialex.shabanovaii@gmail.com>
2026-04-15 13:14:58 +0300

    add state to handler

--- app/telegram_bot/handlers/other.py
+++ app/telegram_bot/handlers/other.py
@@ -1,4 +1,5 @@
 from aiogram import Router
+from aiogram.filters import StateFilter
 from aiogram.types import Message
 from telegram_bot.lexicon.lexicon import LEXICON
 
@@ -8,6 +9,6 @@ other_router = Router()
 
 # Этот хэндлер будет срабатывать на любые ваши сообщения,
 # кроме команд "/start" и "/help"
-@other_router.message()
+@other_router.message(StateFilter(None))
 async def process_other_command(message: Message):
     await message.answer(text=LEXICON["no_post"])
