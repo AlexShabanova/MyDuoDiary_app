3a2c365d5f0648fb3758f4fa081da033e2db6a8c
AlexShabanova <iialex.shabanovaii@gmail.com>
2026-04-16 15:53:36 +0300

    update lexicon: in english now

--- app/telegram_bot/lexicon/lexicon.py
+++ app/telegram_bot/lexicon/lexicon.py
@@ -1,12 +1,19 @@
 LEXICON: dict[str, str] = {
-    "/start": "Привет!\n\nЯ бот для ведения дневника на японском!\n"
-    "Можете написать пост о том, как прошел ваш день,\n"
-    "открыть словарь или отправить команду /help",
-    "/help": "Я перевожу слова, которые вы не знаете",
-    "no_post": "Нажмите на кнопку",
-    "post": "write a post",
-    "jisho_dict": "open jisho dictionary",
-    "tanoshii_dict": "open tanoshii dictionary",
+    "/start": "Hi!👋\n\n I’ll help you keep a diary in <b>Japanese</b>.\n\n"
+    "Write about your day, open the dictionary,"
+    " or use /help for more info.",
+    "/help": "I'm here to help with your Japanese vocabulary🎓\n\n"
+    "Write your post, putting any unknown words <b>inside ( )</b>, and I'll translate them.\n\n"
+    "Happy journaling!🌸",
+    "no_post": "Click the button👇",
+    "post": "write a post 📝",
+    "jisho_dict": "jisho dictionary 📙",
+    "tanoshii_dict": "tanoshii dictionary 📗",
+    "brackets": "Put unfamiliar words in <b>brackets ( )</b>",
+    "saved": "Awesome! Your post is saved 🥳",
+    "no_words": "No words to translate yet🤨\n\n"
+    "Please write unfamiliar words in <b>brackets ( )</b> or just <b>save the post</b>",
+    "exit": "See you next time you want to post!👋",
 }
 
 LEXICON_COMMANDS: dict[str, str] = {
