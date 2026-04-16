3ff27dfd7e54178172e0e5ece8d85607d018a30e
AlexShabanova <iialex.shabanovaii@gmail.com>
2026-04-16 15:52:18 +0300

    delete comments, other_router

--- app/telegram_bot/bot.py
+++ app/telegram_bot/bot.py
@@ -6,47 +6,34 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from handlers import other, user
+from handlers import user
 from telegram_bot.bot_config import BotConfig, load_bot_config
 from telegram_bot.keyboards import set_main_menu
 
-# Инициализируем логгер
 logger = logging.getLogger(__name__)
 
 
-# Функция конфигурирования и запуска бота
 async def main():
-
-    # Загружаем конфиг в переменную config
     # FIXME убрать абсолютный путь - при переносе нужно проверить загрузка в венв переменных из .env (должно вернуть True) dotenv.load_dotenv(dotenv_path='C:\\Users\\user\\PycharmProjects\\MyDuoDiary_app\\app\\.env', override=True)
     config: BotConfig = load_bot_config(
         "C:\\Users\\user\\PycharmProjects\\MyDuoDiary_app\\app\\.env"
     )
 
-    # Задаём базовую конфигурацию логирования
     logging.basicConfig(
         level=logging.getLevelName(level=config.log.level),
         format=config.log.format,
     )
-    # Инициализируем хранилище (создаем экземпляр класса MemoryStorage)
-    storage = MemoryStorage()
 
-    # Инициализируем бот и диспетчер
+    # TODO заменить на Redis
+    storage = MemoryStorage()
     bot = Bot(
         token=config.bot.token,
         storage=storage,
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
     )
     dp = Dispatcher()
-
-    # Настраиваем кнопку Menu
     await set_main_menu(bot)
-
-    # Регистриуем роутеры в диспетчере
     dp.include_router(user.user_router)
-    dp.include_router(other.other_router)
-
-    # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
